532db9362d372bc8c9e6f32d189ce7b48c371a40
Simo Piitulainen <sipesapi@student.jyu.fi>
2022-08-13 22:15:25 +0300

    Add table stage.Teams

---
+++ NHLDW/NHLDW/Tables/stage.Teams.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [stage].[Teams]
+(
+	[id] INT NULL, 
+    [name] VARCHAR(255) NULL, 
+    [link] VARCHAR(1000) NULL, 
+    [venue_name] VARCHAR(255) NULL, 
+    [venue_link] VARCHAR(1000) NULL, 
+    [city] VARCHAR(255) NULL, 
+    [abbreviation] VARCHAR(5) NULL, 
+    [team_name] VARCHAR(255) NULL, 
+    [location_name] VARCHAR(255) NULL, 
+    [firstYearOfPlay] INT NULL, 
+    [division_id] INT NULL, 
+    [division_name] VARCHAR(255) NULL, 
+    [name_short] VARCHAR(255) NULL, 
+    [conference_id] INT NULL, 
+    [fanchise_id] INT NULL, 
+    [officialSiteUrl] VARCHAR(1000) NULL, 
+    [active] VARCHAR(16) NULL 
+)
